3128b70b32033343a8ee8c64b06c7676325a2b3e
Farida Idrisova <faridaIdrisova@mail.ru>
2020-10-12 16:15:12 +0300

    Applied the logic

---
+++ lecture02/src/main/java/ru/atom/geometry/Bar.java
@@ -0,0 +1,68 @@
+package ru.atom.geometry;
+
+public class Bar implements Collider {
+    private Point point1;
+
+    private Point point2;
+
+    private boolean isPointInsideBar(Point point, Bar bar) {
+        int x = point.getX();
+        int y = point.getY();
+
+        return x >= bar.point1.getX() && y >= bar.point1.getY() && x <= bar.point2.getX() && y <= bar.point2.getY();
+    }
+
+    private void SetupPoints() {
+        if (this.point1.getX() > this.point2.getX()) {
+            int tmp = this.point1.getX();
+            this.point1.setX(this.point2.getX());
+            this.point2.setX(tmp);
+        }
+
+        if (this.point1.getY() > this.point2.getY()) {
+            int tmp = this.point1.getY();
+            this.point1.setY(this.point2.getY());
+            this.point2.setY(tmp);
+        }
+    }
+
+    public Bar(int xTop, int yTop, int xBottom, int yBottom) {
+        this.point1 = new Point(xTop, yTop);
+        this.point2 = new Point(xBottom, yBottom);
+
+        this.SetupPoints();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Bar) {
+            Bar bar = (Bar) other;
+            Point otherPoint1 = new Point(bar.point2.getX(), bar.point1.getY());
+            Point otherPoint2 = new Point(bar.point1.getX(), bar.point2.getY());
+
+            if (this.isPointInsideBar(bar.point1, this)
+                    || this.isPointInsideBar(bar.point2, this)
+                    || this.isPointInsideBar(otherPoint1, this)
+                    || this.isPointInsideBar(otherPoint2, this)) {
+                return true;
+            } else return bar.point2.getX() <= this.point2.getX()
+                    && bar.point1.getX() >= this.point1.getX()
+                    && bar.point2.getY() >= this.point2.getY()
+                    && bar.point1.getY() <= this.point1.getY()
+                    || bar.point2.getY() <= this.point2.getY()
+                    && bar.point1.getY() >= this.point1.getY()
+                    && bar.point2.getX() >= this.point2.getX()
+                    && bar.point1.getX() <= this.point1.getX();
+        } else if (other instanceof Point) {
+            Point otherPoint = (Point) other;
+            return this.isPointInsideBar(otherPoint, this);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        return this.equals(other);
+    }
+}
--- lecture02/src/main/java/ru/atom/geometry/Point.java
+++ lecture02/src/main/java/ru/atom/geometry/Point.java
@@ -3,9 +3,32 @@ package ru.atom.geometry;
 /**
  * Template class for
  */
-public class Point /* super class and interfaces here if necessary */ {
+public class Point implements Collider /* super class and interfaces here if necessary */ {
+    public Point(int x, int y) {
+        this.X = x;
+        this.Y = y;
+    }
+
     // fields
     // and methods
+    public int X;
+    public int Y;
+
+    public void setX(int x) {
+        this.X = x;
+    }
+
+    public void setY(int y) {
+        this.Y = y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
 
     /**
      * @param o - other object to check equality with
@@ -20,6 +43,15 @@ public class Point /* super class and interfaces here if necessary */ {
         Point point = (Point) o;
 
         // your code here
-        throw new UnsupportedOperationException();
+        if (this.X == point.X && this.Y == point.Y) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isColliding(Collider other) {
+        return this.equals(other);
     }
 }
